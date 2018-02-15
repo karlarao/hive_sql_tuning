4fc4774307d0d85e99fdf983c5f0939cd7051173
karlarao <karlarao@gmail.com>
2018-02-15 15:15:59 -0800

    added sample query

---
+++ lipwig_explain_plan/testquery.sql
@@ -0,0 +1,13 @@
+-- explain plan/instrumentation settings
+set hive.log.explain.output=true;
+set hive.explain.user=true;
+set hive.tez.exec.print.summary=true;
+
+-- hive params to test
+--set hive.auto.convert.join = false;
+
+explain formatted
+select count(employees.department_id)
+from employees 
+join departments 
+on employees.department_id = departments.department_id;
