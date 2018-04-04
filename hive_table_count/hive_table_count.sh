9f9183ad643591d25dc47d51d62fe6711e050855
karlarao <karlarao@gmail.com>
2018-04-04 18:44:00 -0400

    table count edits

--- hive_table_count/hive_table_count.sh
+++ hive_table_count/hive_table_count.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-rm -f HiveTableCount.txt
-wait
+
 cat tableNames.txt |while read LINE
    do
    echo -e $LINE >> HiveTableCount.txt
@@ -13,3 +12,18 @@ awk '!(NR%2){print$0p}{p=" "$0}' combinecount.txt > finalcount.txt
 
 echo "Table Count Done..."
 cat finalcount.txt
+
+
+# cat hivesize.sql
+# describe formatted restou_derived.dc_exceptions_mapping;
+# describe formatted restou_derived.wdt_delayed_bill;
+# describe formatted restou_derived.wdt_delayed_bill;
+
+
+# less hivesize.txt | egrep -i "location|totalsize"
+# Location:               hdfs://dev/sdge/derived/restou/dc_exceptions_mapping
+#         totalSize               76873
+# Location:               hdfs://dev/sdge/derived/wdt/delayed_bill
+#         totalSize               946983
+# Location:               hdfs://dev/sdge/derived/wdt/delayed_bill
+#         totalSize               946983
